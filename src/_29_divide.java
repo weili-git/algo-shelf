1e4411f8449e129027b5ce764d07ff4cb2438d98
liwei <overshadow.lw@gmail.com>
2022-05-11 11:58:16 +0800

    5.11

--- src/_29_divide.java
+++ src/_29_divide.java
@@ -2,7 +2,22 @@ import javax.swing.plaf.synth.SynthLookAndFeel;
 
 public class _29_divide {
     public static int divide(int dividend, int divisor) {
-
+        if(dividend==0) return 0;
+        if(dividend==Integer.MIN_VALUE && divisor==-1){
+            return Integer.MAX_VALUE;
+        }
+        boolean negative;
+        negative = (dividend ^ divisor) < 0;    // 判断符号是否相同
+        long t = Math.abs((long) dividend);
+        long d = Math.abs((long) divisor);
+        int res = 0;
+        for(int i=31;i>=0;i--){
+            if((t>>i)>=d){
+                res+=1<<i;
+                t-=d<<i;
+            }
+        }
+        return negative?-res:res;
     }
 
     public static void main(String[] args) {
---
+++ src/_31_nextPermutation.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class _31_nextPermutation {
+    public static void nextPermutation(int[] nums) {
+        int n = nums.length;
+        if(n<=1) return;
+        for(int i=n-1;i>0;i--){
+            if(nums[i] > nums[i-1]){    //  1, 2, 5, 4, 3
+                int j;
+                for(j=n-1;j>i-1;j--){   // first bigger than nums[i-1], swap them.
+                    if(nums[j]>nums[i-1]) break;
+                }
+                int tmp = nums[j];    //  swap nums[i-1] and min of tail=[5, 4, 3]
+                nums[j] = nums[i-1];
+                nums[i-1] = tmp;
+                Arrays.sort(nums, i, n);    //  overwrite
+                return;
+            }
+        }
+        Arrays.sort(nums);
+    }
+
+    public static void main(String[] args) {
+        // 2, 3, 1
+        int[] nums = {1, 2, 3, 4};
+        nextPermutation(nums);
+        for(int i=0;i<nums.length;i++){
+            System.out.printf("%d, ", nums[i]);
+        }
+    }
+}
